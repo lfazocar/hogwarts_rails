a927a1ba64c79cb79dfb6fbf071f1600df971c78
Felipe <lfazocar@users.noreply.github.com>
2023-07-02 20:55:44 -0400

    feat: Added Character model and its seed and Pages controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+seed_amount = 50
+
+puts "Creating #{seed_amount} characters..."
+seed_amount.times do
+    Character.create(
+        name: Faker::Movies::HarryPotter.character,
+        location: Faker::Movies::HarryPotter.location,
+        house: Faker::Movies::HarryPotter.house
+    )
+end
